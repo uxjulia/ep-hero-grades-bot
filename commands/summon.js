3aa439d76a846e2adc9791433b1eb9c1906b644d
Julia N <julia@8thsq.co>
2018-10-14 19:44:29 -0400

    Refactor commands

--- commands/summon.js
+++ commands/summon.js
@@ -3,6 +3,11 @@ const mt = Random.engines.mt19937()
 const random = new Random(mt.autoSeed())
 const heroes = require('../heroes')
 
+//TODO: Add appearance rate for Elemental and Atlantis summons.
+/**
+ * The appearance rate of heroes for an Epic summon
+ * @type {{hotm: number, legendary: number, epic: number, rare: number}}
+ */
 const rate = {
   hotm: 1.3,
   legendary: 2.5,
@@ -10,9 +15,15 @@ const rate = {
   rare: 100
 }
 
+/**
+ * Represents a summon
+ * @constructor
+ * @param {string} option - One of [epic, elemental, atlantis]
+ * @param {string} element - One of [fire, ice, holy, dark, nature]
+ */
 class Summon {
-  constructor(type = 'elemental', element = '') {
-    this.type = type
+  constructor(option = 'epic', element = '') {
+    this.option = option
     this.element = element
   }
 
@@ -37,26 +48,40 @@ class Summon {
     return Random.bool(.013)(mt)
   }
 
-  get hero() {
-    let r = Summon.rarity() //?
+  static getHero() {
+    let r = Summon.rarity()
     let length = heroes[r].length
     const rdm = Summon.randomize(0, length)
-    let hero = heroes[r][rdm] //?
-    const allowBonus = Summon.allowBonusDraw() //?
-    return `
-    You summoned ${r === 'epic' ? 'an' : 'a'} **${r}** hero: **${hero}**!
-    ${allowBonus === true ? 'Congrats! You also drew the Hero of the Month!' : 'Sorry, you didn\'t pull the Hero of the Month'}`
+    let hero = `${heroes[r][rdm]} (**${r}**)`
+    let allowBonus = Summon.allowBonusDraw()
+    let result = hero
+    if (allowBonus !== false) {
+      result = hero.concat(`+ BONUS HOTM`)
+    }
+    return result
+  }
+
+  pull() {
+    const msg = `Here is the result of your summon: \n`
+    const heroes = []
+    if (this.option === '10') {
+      let x = 10
+      for (let i = 0; i < x; i++) {
+        heroes.push(`${i + 1}) ${Summon.getHero()}`)
+      }
+      return msg + heroes.join('\n')
+    }
+    heroes.push(Summon.getHero())
+    return msg + heroes
   }
 }
 
 module.exports = {
   name: 'summon',
   description: 'Test your luck and summon a hero without actually summoning a hero!',
-  args: false,
-  execute(message) {
-    // if (args.length) {
-    const summon = new Summon('elemental')
-    message.reply(summon.hero)
-    // }
+  args: true,
+  execute(message, args) {
+    const summon = new Summon(args[0], args[1])
+    message.reply(summon.pull())
   }
 }
