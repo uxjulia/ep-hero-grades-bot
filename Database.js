68a3cb44249b4ee866045b3d2558527cf30d810f
Julia N <julia@8thsq.co>
2018-12-07 14:49:13 -0500

    Improve error handling

--- Database.js
+++ Database.js
@@ -7,6 +7,26 @@ const defenseBase = base('Defense Grades')
 const offenseBase = base('Offense Grades')
 const overview = base('Overview')
 
+function getHeroData(hero, record, message) {
+  const data = {};
+  data.heroName = hero
+  data.overallGrade = record.get('Overall')
+  data.oTitan = record.get('Overall Titan')[0]
+  data.oDefense = record.get('Overall Defense')[0]
+  data.oOffense = record.get('Overall Offense')[0]
+  data.element = record.get('Element')[0]
+  data.stars = record.get('Stars')[0]
+  data.power = record.get('Power')[0]
+  data.attack = record.get('Attack')[0]
+  data.defense = record.get('Defense')[0]
+  data.health = record.get('Health')[0]
+  data.special = record.get('Special')[0]
+  data.specialName = record.get('Special Name')[0]
+  data.mana = record.get('Mana')[0]
+  data.limited = record.get('Limited')[0] === 'TRUE' ? 'Yes' : 'No'
+  return Logger.success['info'](message, data)
+}
+
 function getInfo(hero, message) {
   let count = 0
   overview.select({
@@ -18,31 +38,19 @@ function getInfo(hero, message) {
         let heroName = record.get('Name')[0]
         if (heroName.toLowerCase() === hero) {
           count++
-          const data = {}
-          data.heroName = heroName
-          data.overallGrade = record.get('Overall')
-          data.oTitan = record.get('Overall Titan')[0]
-          data.oDefense = record.get('Overall Defense')[0]
-          data.oOffense = record.get('Overall Offense')[0]
-          data.element = record.get('Element')[0]
-          data.stars = record.get('Stars')[0]
-          data.power = record.get('Power')[0]
-          data.attack = record.get('Attack')[0]
-          data.defense = record.get('Defense')[0]
-          data.health = record.get('Health')[0]
-          data.special = record.get('Special')[0]
-          data.specialName = record.get('Special Name')[0]
-          data.mana = record.get('Mana')[0]
-          data.limited = record.get('Limited')[0] === 'TRUE' ? 'Yes' : 'No'
-          Logger.success['info'](message, data)
+          try {
+            getHeroData(heroName, record, message)
+          }
+          catch (err) {
+            Logger.error(hero, err, message)
+          }
         }
       })
       fetchNextPage()
     },
     function done(err) {
       if (err) {
-        Logger.error(hero, err)
-        return err
+        Logger.error(hero, err, message)
       }
       if (count === 0) {
         Logger.noData(message, hero)
@@ -78,7 +86,6 @@ function getTitan(hero, message) {
     function done(err) {
       if (err) {
         Logger.error(hero, err)
-        return err
       }
       if (count === 0) {
         Logger.noData(message, hero)
@@ -115,7 +122,6 @@ function getDefense(hero, message) {
     function done(err) {
       if (err) {
         Logger.error(hero, err)
-        return err
       }
       if (count === 0) {
         Logger.noData(message, hero)
@@ -151,7 +157,6 @@ function getOffense(hero, message) {
     function done(err) {
       if (err) {
         Logger.error(hero, err)
-        return err
       }
       if (count === 0) {
         Logger.noData(message, hero)
