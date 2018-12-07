2e5076c6257803e8eab0c57e3c37571e77b046be
Julia N <julia@8thsq.co>
2018-12-07 13:56:38 -0500

    Clean up code Accurately tag username

--- Logger.js
+++ Logger.js
@@ -1,16 +1,7 @@
-const Discord = require('discord.js');
-const Canvas = require('canvas');
-
-const canvas = Canvas.createCanvas(432, 680);
-const ctx = canvas.getContext('2d');
-
-
-module.exports = {
-  success: {
-    info: function (message, data) {
-      console.log('Successfully retrieved info for', data.heroName)
-      message.reply(
-        `Here's some information on ${data.heroName}:
+const info = function (message, data) {
+  console.log('Successfully retrieved info for', data.heroName)
+  message.reply(
+    `Here's some information on ${data.heroName}:
 **Element**: ${data.element}
 **Stars**: ${data.stars}
 **Limited Availability?**: ${data.limited}
@@ -24,12 +15,13 @@ Defense grade: **${data.oDefense}**
 Offense grade: **${data.oOffense}**
 __
 ${data.heroName}'s overall grade is **${data.overallGrade}**`
-      )
-    },
-    titan: function (message, data) {
-      console.log('Successfully retrieved titan data for', data.heroName)
-      message.reply(
-        `Here are ${data.heroName}'s **titan** grades:
+  )
+};
+
+const titan = function (message, data) {
+  console.log('Successfully retrieved titan data for', data.heroName)
+  message.reply(
+    `Here are ${data.heroName}'s **titan** grades:
 
 **Stamina**: ${data.stamina}
 **Passive**: ${data.passive}
@@ -38,12 +30,13 @@ ${data.heroName}'s overall grade is **${data.overallGrade}**`
 **Versatility**: ${data.versatility}
 __
 ${data.heroName}'s overall **titan** grade is **${data.overallGrade}**`
-      )
-    },
-    defense: function (message, data) {
-      console.log('Successfully retrieved defense data for', data.heroName)
-      message.reply(
-        `Here are ${data.heroName}'s **defense** grades:
+  )
+};
+
+const defense = function (message, data) {
+  console.log('Successfully retrieved defense data for', data.heroName)
+  message.reply(
+    `Here are ${data.heroName}'s **defense** grades:
 
 **Speed**: ${data.speed}
 **Effect**: ${data.effect}
@@ -53,12 +46,13 @@ ${data.heroName}'s overall **titan** grade is **${data.overallGrade}**`
 **Support**: ${data.support}
 __
 ${data.heroName}'s overall **defense** grade is **${data.overallGrade}**`
-      )
-    },
-    offense: function (message, data) {
-      console.log('Successfully retrieved offense data for', data.heroName)
-      message.reply(
-        `Here are ${data.heroName}'s **offense** grades:
+  )
+};
+
+const offense = function (message, data) {
+  console.log('Successfully retrieved offense data for', data.heroName)
+  message.reply(
+    `Here are ${data.heroName}'s **offense** grades:
 
 **Speed**: ${data.speed}
 **Effect**: ${data.effect}
@@ -67,19 +61,27 @@ ${data.heroName}'s overall **defense** grade is **${data.overallGrade}**`
 **War**: ${data.war}
 __
 ${data.heroName}'s overall **offense** grade is **${data.overallGrade}**`
-      )
-    },
-    withImage: function(image, message, isUpdated) {
-      console.log('Successfully retrieved image')
-      const messageWithNote = 'Note: This image needs to be updated'
-      return isUpdated===false ? message.reply(messageWithNote, image) : message.reply(image)
-    }
-  },
-  error(data) {
-    console.error(`An error occurred while retrieving ${data}`)
-  },
-  noData(message, data) {
-    console.error(`No record found for ${data}`)
-    message.reply('Uh oh. I can\'t seem to find that hero. If I should know this hero, please let my master @Av0 know to update my brains.')
-  }
-}
+  )
+};
+
+const withImage = function(image, message, isUpdated) {
+  console.log('Successfully retrieved image')
+  const messageWithNote = 'Note: This image needs to be updated'
+  return isUpdated===false ? message.reply(messageWithNote, image) : message.reply(image)
+};
+
+const success = {info, titan, defense, offense, withImage };
+
+const error =  function(data) {
+  console.error(`An error occurred while retrieving ${data}`)
+};
+
+const noData = function(message, data) {
+  console.error(`No record found for ${data}`)
+  message.reply('Uh oh. I can\'t seem to find that hero. If I should know this hero, please let my master <@!342706933389852672> know to update my brains.')
+};
+
+module.exports = { success, error, noData };
+
+
+// TODO: Add a command that allows users to add/alert of new heroes.
