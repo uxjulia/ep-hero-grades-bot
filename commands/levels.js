38e1768e7cfa4c71c540dbf476bd54fa05e5bb57
Julia Nguyen <julia@8thsq.co>
2019-05-30 11:26:11 -0400

    add additional levels

--- commands/levels.js
+++ commands/levels.js
@@ -1,23 +1,24 @@
-const {log} = require('../Utils');
+const { log } = require("../Utils");
 
 const data = {
-  fire: '20-4, 19-6, 19-4, 11-6, 6-8',
-  ice: '8-7',
-  nature: '7-5',
-  holy: '10-6, 12-9',
-  dark: '7-4'
-}
+  fire: "20-4, 19-6, 19-4, 11-6, 6-8",
+  ice: "8-7, 14-5",
+  nature: "7-5",
+  holy: "10-6, 12-9",
+  dark: "7-4, 5-5"
+};
 
 module.exports = {
-  name: 'levels',
-  aliases: 'level',
-  description: 'Get best levels for filling elemental chests',
+  name: "levels",
+  aliases: "level",
+  description: "Get best levels for filling elemental chests",
   args: true,
   execute(message, args) {
     let element = args[0].toUpperCase();
     let levels = data[args[0].toLowerCase()];
-    message.channel.send(`Best level(s) for ${element} monsters: ${levels}`)
+    message.channel
+      .send(`Best level(s) for ${element} monsters: ${levels}`)
       .then(() => log(`Successfully sent reply for ${element} monsters`))
-      .catch(error => console.error(error.message))
+      .catch(error => console.error(error.message));
   }
-}
+};
